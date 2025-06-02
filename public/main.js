42131c1cbb1990bb14dced11278a80f4f12a7cdb
kate hollenbach <kjhollen@gmail.com>
2025-06-02 16:55:22 -0700

    updated with better comments & to be more robust

--- public/main.js
+++ public/main.js
@@ -1,20 +1,42 @@
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', async event => {
-    const wish = document.getElementById('wish').value;
-    const data = {
-        wish: wish
-    };
+/* sends a POST request with our wish to the server when
+ * the submit button is pressed, or the user presses the enter
+ * key in the text input box.
+ */
 
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    };
+const form = document.getElementById('wishForm');
+const wish = document.getElementById('wish');
 
-    const response = await fetch("/wish", options);
-    const json = await response.json();
-    console.log(json);
-    e.preventDefault();
+/*
+ * listen for events on the form, rather than the button,
+ * so that both the button press and pressing enter work to
+ * submit the form.
+ */
+form.addEventListener('submit', async event => {
+  
+  // do this first so the page doesn't refresh!
+  event.preventDefault();
+
+  // these are are Request options:
+  // - the type of request is POST because we want to send data to the server
+  // - the data we are sending is JSON, so we need to add a content-type header
+  // - and encode the relevant data as JSON
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      wish: wish.value
+    })
+  };
+  
+  // send the request and await a response
+  const response = await fetch("/wish", options);
+  
+  // parse the response into JSON
+  const json = await response.json();
+  console.log(json);
+
+  // clear the wish value!
+  wish.value = "";
 });
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = Number(process.env.PORT || 3000);
 const server = app.listen(port);
 
+// use this file as the database
 const dbOptions = {
   filename: 'database.db'
 };
@@ -15,17 +16,25 @@ console.log(`Server is listening on port ${port}`);
 database.loadDatabase();
 
 app.use(express.static('public'));
+
+// this prevents people from spamming our server with excessively
+// long responses
 app.use(express.json({limit: '1mb'}));
 
+/* when we receive a POST request, the user is sending us data */
 app.post('/wish', (request, response) => {
   console.log("POST Request Received");
+  
   const data = request.body;
-  console.log(data);
-
+  
+  // add a time stamp along with the data we received
   const timestamp = Date.now();
   data.timestamp = timestamp;
-    
+
+  // save the data in the database
   database.insert(data);
+  
+  // send a response to the client
   response.json({
     status: 'success',
     wish: data.wish,
@@ -33,8 +42,14 @@ app.post('/wish', (request, response) => {
   });
 });
 
+/* GET requests: server sends the client all the wishes from
+ * the database
+ */
 app.get('/wish', (request, response) => {
-  database.find({}, (error, data) => {
+  // grab all the data (since it's not too much!) and then 
+  // sort it by timestamp in reverse chronological order
+  // (newest first).
+  database.find({}).sort({timestamp: -1}).exec((error, data) => {
     if (error) {
       response.end();
       return;
